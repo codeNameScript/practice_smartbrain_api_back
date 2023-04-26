eddf767e958d1652b84f0fa6d5f778856ac2c551
codeNameScript <130048913+codeNameScript@users.noreply.github.com>
2023-04-26 13:48:10 +0900

    changed port

--- server.js
+++ server.js
@@ -68,9 +68,7 @@ app.put('/image', (req, res) => image.handleImage(req, res, db))
 
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res))
 
-const PORT = process.env.PORT;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`app is running on port 3001 ${PORT}`);
 })
-
-console.log(process.env)
